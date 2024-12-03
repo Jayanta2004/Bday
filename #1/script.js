e3852d8233d405953b846d8ec4062198ad79a55a
JAYANTA GHOSH <82716331+Jayanta2004@users.noreply.github.com>
2024-12-04 02:25:04 +0530

    Delete #1 directory

--- #1/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const colors = ["#ffcccb", "#ffefd5", "#ff6f61"];
-    let index = 0;
-
-    setInterval(() => {
-        document.body.style.background = colors[index];
-        index = (index + 1) % colors.length;
-    }, 3000);
-});
